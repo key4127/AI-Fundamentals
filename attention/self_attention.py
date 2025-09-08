6a48f268fcc597d86786a720cade02ec26ec1cc0
key4127 <2827092384@qq.com>
2025-09-08 17:10:30 +0800

    complete self attention with mask

--- attention/self_attention.py
+++ attention/self_attention.py
@@ -2,6 +2,7 @@ from math import sqrt
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 class SelfAttention(nn.Module):
 
@@ -22,7 +23,7 @@ class SelfAttention(nn.Module):
         self.linear_v = nn.Linear(dim_in, dim_v, False)
         self._norm_fact = 1 / sqrt(dim_k)
 
-    def forward(self, x):
+    def forward(self, x, causal_mask=False):
         """
         x is [batch, seq_length, dim_in]
         output is [batch, seq_length, dim_v]
@@ -30,11 +31,17 @@ class SelfAttention(nn.Module):
         batch, n, dim_in = x.shape
         assert dim_in == self.dim_in
 
-        q = self.linear_q(x)
+        q = self.linear_q(x) # q is [batch, seq_length, dim_k]
         k = self.linear_k(x)
         v = self.linear_v(x)
 
+        # transpose: transform k from [batch, seq_length, dim_k] to [batch, dim_k, seq_length]
         dist = torch.bmm(q, k.transpose(1, 2)) * self._norm_fact
+    
+        if causal_mask:
+            mask = torch.tril(torch.ones(n, n))
+            dist = dist.masked_fill(mask == 0, -np.Inf)
+
         dist = torch.softmax(dist, dim=-1)
 
         att = torch.bmm(dist, v)
