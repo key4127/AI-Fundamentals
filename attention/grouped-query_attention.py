2d0dcd4b79ee0b047bd363e4a5d8e727fe1f7500
key4127 <2827092384@qq.com>
2025-09-25 12:23:51 +0800

    add comments for GQA

--- attention/grouped-query_attention.py
+++ attention/grouped-query_attention.py
@@ -31,11 +31,20 @@ class GroupedQueryAttention(nn.Module):
         k = self.linear_k(x).reshape(batch, seq_len, ng, dk).transpose(1, 2)
         v = self.linear_v(x).reshape(batch, seq_len, ng, dv).transpose(1, 2)
 
+        # k: [batch, ng(4), seq_len, dk]
+
         k = k.repeat_interleave(nh // ng, dim=1)
         v = v.repeat_interleave(nh // ng, dim=1)
 
+        # q: [batch, num_heads, seq_len, dq]
+        # k: [batch, num_heads, seq_len, dk] -> [batch, num_heads, dk, seq_len]
+
         dist = torch.matmul(q, k.transpose(2, 3)) * self._norm_fact
         dist = torch.softmax(dist, dim=-1)
+
+        # dist: [batch, num_heads, seq_len, seq_len]
+        # v: [batch, num_heads, seq_len, dv]
+        # mat: [batch, num_heads, seq_len, dv]
 
         att = torch.matmul(dist, v)
         att = att.transpose(1, 2).reshape(batch, seq_len, self.dim_v)
